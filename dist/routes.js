d0442c72c5d76365164fe8c41694257861920477
Miguel <miguellukas52@gmail.com>
2020-10-24 13:43:45 -0300

    .

--- dist/routes.js
+++ dist/routes.js
@@ -14,18 +14,15 @@ router.get("/matters", (request, response) => {
 router.post("/matters", (request, response) => {
     return CreateMatter_1.createMatterController.handle(request, response);
 });
-router.get("/matters", (request, response) => {
-    return GetMatter_1.getMatterController.find(request, response);
-});
 router.get("/matters/:name", (request, response) => {
     return GetMatter_1.getMatterController.handle(request, response);
 });
 router.get("/tasks", (request, response) => {
     return GetTask_1.getTasksController.find(request, response);
 });
-router.get("/tasks/:name", (request, response) => {
-    return GetTask_1.getTasksController.handle(request, response);
-});
 router.post("/tasks", (request, response) => {
     return CreateTask_1.createTaskController.handle(request, response);
+});
+router.get("/tasks/:name", (request, response) => {
+    return GetTask_1.getTasksController.handle(request, response);
 });
--- src/routes.ts
+++ src/routes.ts
@@ -15,10 +15,6 @@ router.post("/matters", (request, response) => {
   return createMatterController.handle(request, response);
 });
 
-router.get("/matters", (request, response) => {
-  return getMatterController.find(request, response);
-});
-
 router.get("/matters/:name", (request, response) => {
   return getMatterController.handle(request, response);
 });
@@ -27,12 +23,12 @@ router.get("/tasks", (request, response) => {
   return getTasksController.find(request, response);
 });
 
-router.get("/tasks/:name", (request, response) => {
-  return getTasksController.handle(request, response);
-});
-
 router.post("/tasks", (request, response) => {
   return createTaskController.handle(request, response);
+});
+
+router.get("/tasks/:name", (request, response) => {
+  return getTasksController.handle(request, response);
 });
 
 export { router };
