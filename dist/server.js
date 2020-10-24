f45a87a6ac64f9098288fa52eb5c9d0352fea979
Miguel <miguellukas52@gmail.com>
2020-10-24 13:40:00 -0300

    .

--- dist/server.js
+++ dist/server.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./database/connection");
 const app_1 = require("./app");
-app_1.app.listen(3333);
+app_1.app.listen(process.env.PORT || 3333);
--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import "./database/connection";
 
 import { app } from "./app";
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
